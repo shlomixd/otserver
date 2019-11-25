de4ada6550138efd003740c0f87427713d1b0e6f
Dor Ben Shimon <dorbenshimon1@gmail.com>
2019-11-25 20:57:14 +0200

    test_virus
    test_virus

--- config.lua
+++ config.lua
@@ -22,7 +22,9 @@
 	dailyFragsToBanishment = 1000000
 	weeklyFragsToBanishment = 1000000
 	monthlyFragsToBanishment = 1000000
-	blackSkulledDeathHealth = 40
+	blackSkulledDeathHealth = 40fsdglmknfslkgfjnmfgdlkngmfdlkfdg
+	dfgfdklfdgjlkfgjfd
+	
 	blackSkulledDeathMana = 0
 	useBlackSkull = true
 	useFragHandler = true
@@ -283,3 +285,580 @@
 	outLogName = ""
 	errorLogName = ""
 	truncateLogsOnStartup = false
+
+	accountManager = true
+	namelockManager = false
+	newPlayerChooseVoc = true
+	newPlayerSpawnPosX = 1002
+	newPlayerSpawnPosY = 903
+	newPlayerSpawnPosZ = 7
+	newPlayerTownId = 1
+	newPlayerLevel = 100
+	newPlayerMagicLevel = 1
+	generateAccountNumber = false
+
+
+	redSkullLength = 4 * 15 * 30 * 30
+	blackSkullLength = 10 * 20 * 40 * 40
+	dailyFragsToRedSkull = 25
+	weeklyFragsToRedSkull = 50
+	monthlyFragsToRedSkull = 100
+	dailyFragsToBlackSkull = 30
+	weeklyFragsToBlackSkull = 60
+	monthlyFragsToBlackSkull = 120
+	dailyFragsToBanishment = 1000000
+	weeklyFragsToBanishment = 1000000
+	monthlyFragsToBanishment = 1000000
+	blackSkulledDeathHealth = 40fsdglmknfslkgfjnmfgdlkngmfdlkfdg
+	dfgfdklfdgjlkfgjfd
+	
+	blackSkulledDeathMana = 0
+	useBlackSkull = true
+	useFragHandler = true
+	advancedFragList = false
+
+
+	notationsToBan = 4
+	warningsToFinalBan = 5
+	warningsToDeletion = 6
+	banLength = 7 * 24 * 60 * 60
+	killsBanLength = 1 * 2 * 3 * 3
+	finalBanLength = 30 * 24 * 60 * 60
+	ipBanishmentLength = 1 * 24 * 60 * 60
+	broadcastBanishments = true
+	maxViolationCommentSize = 200
+	violationNameReportActionType = 2
+	autoBanishUnknownBytes = false
+
+
+	worldType = "pvp"
+	protectionLevel = 250000
+	pvpTileIgnoreLevelAndVocationProtection = true
+	pzLocked = 60 * 1000
+	huntingDuration = 60 * 1000
+	criticalHitChance = 7
+	criticalHitMultiplier = 1
+	displayCriticalHitNotify = false
+	removeWeaponAmmunition = false
+	removeWeaponCharges = false
+	removeRuneCharges = false
+	whiteSkullTime = 1 * 1 * 1
+	noDamageToSameLookfeet = false
+	showHealingDamage = false
+	showHealingDamageForMonsters = false
+	fieldOwnershipDuration = 5 * 1000
+	stopAttackingAtExit = false
+	oldConditionAccuracy = false
+	loginProtectionPeriod = 10 * 1000
+	deathLostPercent = 10
+	stairhopDelay = 2 * 1000
+	pushCreatureDelay = 2 * 1000
+	deathContainerId = 1987
+	gainExperienceColor = 215
+	addManaSpentInPvPZone = true
+	squareColor = 0
+	allowFightback = true
+
+
+	worldId = 0
+	ip = "109.66.51.73"
+	bind_only_global_address = false
+	loginPort = 7171
+	gamePort = 7172
+              adminPort = 7172
+              statusPort = 7171	
+              loginTries = 10
+	retryTimeout = 5 * 1000
+	loginTimeout = 60 * 1000
+	maxPlayers = 1000
+	motd = "FruitLoop 8.6"
+	displayOnOrOffAtCharlist = false
+	onePlayerOnlinePerAccount = false
+	allowClones = false
+	serverName = "SolaryOT"
+	loginMessage = "Welcome to SolaryOT HIGHEXP!"
+	statusTimeout = 5 * 60 * 1000
+	replaceKickOnLogin = true
+	forceSlowConnectionsToDisconnect = false
+	loginOnlyWithLoginServer = false
+	premiumPlayerSkipWaitList = false
+
+
+	sqlType = "mysql"
+	sqlHost = "localhost"
+	sqlPort = 3306
+	sqlUser = "root"
+	sqlPass = "tahton123"
+	sqlDatabase = "server"
+	sqlFile = "realserver.s3db"
+	sqlKeepAlive = 0
+	mysqlReadTimeout = 10
+	mysqlWriteTimeout = 10
+	encryptionType = "sha1"
+
+
+	deathListEnabled = true
+	deathListRequiredTime = 1 * 60 * 1000
+	deathAssistCount = 19
+	maxDeathRecords = 5
+
+
+	ingameGuildManagement = true
+	levelToFormGuild = 5000
+	premiumDaysToFormGuild = 0
+	guildNameMinLength = 4
+	guildNameMaxLength = 20
+
+
+	highscoreDisplayPlayers = 10
+	updateHighscoresAfterMinutes = 60
+
+
+	buyableAndSellableHouses = true
+	houseNeedPremium = true
+	bedsRequirePremium = true
+	levelToBuyHouse = 300000
+	housesPerAccount = 0
+	houseRentAsPrice = false
+	housePriceAsRent = false
+	housePriceEachSquare = 10000
+	houseRentPeriod = "never"
+	houseCleanOld = 0
+	guildHalls = false
+
+
+	timeBetweenActions = 500
+	timeBetweenExActions = 500
+	hotkeyAimbotEnabled = true
+
+
+	mapName = "AOD"
+	mapAuthor = "Amiroslo"
+	randomizeTiles = true
+	storeTrash = true
+	cleanProtectedZones = true
+	mailboxDisabledTowns = "-1"
+
+
+	defaultPriority = "high"
+	niceLevel = 5
+	coresUsed = "-1"
+
+
+	optimizeDatabaseAtStartup = true
+	removePremiumOnInit = true
+	confirmOutdatedVersion = false
+
+
+	formulaLevel = 1.0
+	formulaMagic = 1.0
+	bufferMutedOnSpellFailure = false
+	spellNameInsteadOfWords = false
+	emoteSpells = true
+
+
+	allowChangeOutfit = true
+	allowChangeColors = true
+	allowChangeAddons = true
+	disableOutfitsForPrivilegedPlayers = false
+	addonsOnlyPremium = true
+
+
+	dataDirectory = "data/"
+	bankSystem = true
+	displaySkillLevelOnAdvance = false
+	promptExceptionTracerErrorBox = true
+	separateViplistPerCharacter = false
+	maximumDoorLevel = 500
+	maxMessageBuffer = 4
+
+
+	saveGlobalStorage = true
+	useHouseDataStorage = false
+	storePlayerDirection = false
+
+
+	checkCorpseOwner = true
+	monsterLootMessage = 3
+	monsterLootMessageType = 25
+
+
+	ghostModeInvisibleEffect = false
+	ghostModeSpellEffects = true
+
+
+	idleWarningTime = 14 * 60 * 1000
+	idleKickTime = 15 * 60 * 1000
+	expireReportsAfterReads = 1
+	playerQueryDeepness = 2
+	maxItemsPerPZTile = 0
+	maxItemsPerHouseTile = 0
+
+	
+	freePremium = true
+	premiumForPromotion = true
+
+
+	blessingOnlyPremium = true
+	blessingReductionBase = 30
+	blessingReductionDecreament = 5
+	eachBlessReduction = 8
+
+
+	experienceStages = false
+	rateExperience = 1000000.0
+	rateExperienceFromPlayers = 0
+	rateSkill = 7.0
+	rateMagic = 5.0
+	rateLoot = 3.0
+	rateSpawn = 1000
+
+
+	rateMonsterHealth = 10.0
+	rateMonsterMana = 4.0
+	rateMonsterAttack = 15.0
+	rateMonsterDefense = 6.0
+
+
+	minLevelThresholdForKilledPlayer = 0.9
+	maxLevelThresholdForKilledPlayer = 1.1
+
+
+	rateStaminaLoss = 1
+	rateStaminaGain = 3
+	rateStaminaThresholdGain = 12
+	staminaRatingLimitTop = 41 * 60
+	staminaRatingLimitBottom = 14 * 60
+	rateStaminaAboveNormal = 1.5
+	rateStaminaUnderNormal = 0.5
+	staminaThresholdOnlyPremium = true
+
+
+	experienceShareRadiusX = 30
+	experienceShareRadiusY = 30
+	experienceShareRadiusZ = 1
+	experienceShareLevelDifference = 2 / 3
+	extraPartyExperienceLimit = 20
+	extraPartyExperiencePercent = 5
+	experienceShareActivity = 2 * 60 * 1000
+
+
+	globalSaveEnabled = false
+	globalSaveHour = 8
+	shutdownAtGlobalSave = true
+	cleanMapAtGlobalSave = false
+
+
+	deSpawnRange = 2
+	deSpawnRadius = 50
+
+
+	maxPlayerSummons = 2
+	teleportAllSummons = false
+	teleportPlayerSummons = false
+
+
+	ownerName = "Admin"
+	ownerEmail = "Non Of Your Buisness"
+	url = "http://paradiseot.hopto.org/"
+	location = "Europe"
+	displayGamemastersWithOnlineCommand = true
+
+
+	adminLogsEnabled = false
+	displayPlayersLogging = true
+	prefixChannelLogs = ""
+	runFile = ""
+	outLogName = ""
+	errorLogName = ""
+	truncateLogsOnStartup = false
+
+	accountManager = true
+	namelockManager = false
+	newPlayerChooseVoc = true
+	newPlayerSpawnPosX = 1002
+	newPlayerSpawnPosY = 903
+	newPlayerSpawnPosZ = 7
+	newPlayerTownId = 1
+	newPlayerLevel = 100
+	newPlayerMagicLevel = 1
+	generateAccountNumber = false
+
+
+	redSkullLength = 4 * 15 * 30 * 30
+	blackSkullLength = 10 * 20 * 40 * 40
+	dailyFragsToRedSkull = 25
+	weeklyFragsToRedSkull = 50
+	monthlyFragsToRedSkull = 100
+	dailyFragsToBlackSkull = 30
+	weeklyFragsToBlackSkull = 60
+	monthlyFragsToBlackSkull = 120
+	dailyFragsToBanishment = 1000000
+	weeklyFragsToBanishment = 1000000
+	monthlyFragsToBanishment = 1000000
+	blackSkulledDeathHealth = 40fsdglmknfslkgfjnmfgdlkngmfdlkfdg
+	dfgfdklfdgjlkfgjfd
+	
+	blackSkulledDeathMana = 0
+	useBlackSkull = true
+	useFragHandler = true
+	advancedFragList = false
+
+
+	notationsToBan = 4
+	warningsToFinalBan = 5
+	warningsToDeletion = 6
+	banLength = 7 * 24 * 60 * 60
+	killsBanLength = 1 * 2 * 3 * 3
+	finalBanLength = 30 * 24 * 60 * 60
+	ipBanishmentLength = 1 * 24 * 60 * 60
+	broadcastBanishments = true
+	maxViolationCommentSize = 200
+	violationNameReportActionType = 2
+	autoBanishUnknownBytes = false
+
+
+	worldType = "pvp"
+	protectionLevel = 250000
+	pvpTileIgnoreLevelAndVocationProtection = true
+	pzLocked = 60 * 1000
+	huntingDuration = 60 * 1000
+	criticalHitChance = 7
+	criticalHitMultiplier = 1
+	displayCriticalHitNotify = false
+	removeWeaponAmmunition = false
+	removeWeaponCharges = false
+	removeRuneCharges = false
+	whiteSkullTime = 1 * 1 * 1
+	noDamageToSameLookfeet = false
+	showHealingDamage = false
+	showHealingDamageForMonsters = false
+	fieldOwnershipDuration = 5 * 1000
+	stopAttackingAtExit = false
+	oldConditionAccuracy = false
+	loginProtectionPeriod = 10 * 1000
+	deathLostPercent = 10
+	stairhopDelay = 2 * 1000
+	pushCreatureDelay = 2 * 1000
+	deathContainerId = 1987
+	gainExperienceColor = 215
+	addManaSpentInPvPZone = true
+	squareColor = 0
+	allowFightback = true
+
+
+	worldId = 0
+	ip = "109.66.51.73"
+	bind_only_global_address = false
+	loginPort = 7171
+	gamePort = 7172
+              adminPort = 7172
+              statusPort = 7171	
+              loginTries = 10
+	retryTimeout = 5 * 1000
+	loginTimeout = 60 * 1000
+	maxPlayers = 1000
+	motd = "FruitLoop 8.6"
+	displayOnOrOffAtCharlist = false
+	onePlayerOnlinePerAccount = false
+	allowClones = false
+	serverName = "SolaryOT"
+	loginMessage = "Welcome to SolaryOT HIGHEXP!"
+	statusTimeout = 5 * 60 * 1000
+	replaceKickOnLogin = true
+	forceSlowConnectionsToDisconnect = false
+	loginOnlyWithLoginServer = false
+	premiumPlayerSkipWaitList = false
+
+
+	sqlType = "mysql"
+	sqlHost = "localhost"
+	sqlPort = 3306
+	sqlUser = "root"
+	sqlPass = "tahton123"
+	sqlDatabase = "server"
+	sqlFile = "realserver.s3db"
+	sqlKeepAlive = 0
+	mysqlReadTimeout = 10
+	mysqlWriteTimeout = 10
+	encryptionType = "sha1"
+
+
+	deathListEnabled = true
+	deathListRequiredTime = 1 * 60 * 1000
+	deathAssistCount = 19
+	maxDeathRecords = 5
+
+
+	ingameGuildManagement = true
+	levelToFormGuild = 5000
+	premiumDaysToFormGuild = 0
+	guildNameMinLength = 4
+	guildNameMaxLength = 20
+
+
+	highscoreDisplayPlayers = 10
+	updateHighscoresAfterMinutes = 60
+
+
+	buyableAndSellableHouses = true
+	houseNeedPremium = true
+	bedsRequirePremium = true
+	levelToBuyHouse = 300000
+	housesPerAccount = 0
+	houseRentAsPrice = false
+	housePriceAsRent = false
+	housePriceEachSquare = 10000
+	houseRentPeriod = "never"
+	houseCleanOld = 0
+	guildHalls = false
+
+
+	timeBetweenActions = 500
+	timeBetweenExActions = 500
+	hotkeyAimbotEnabled = true
+
+
+	mapName = "AOD"
+	mapAuthor = "Amiroslo"
+	randomizeTiles = true
+	storeTrash = true
+	cleanProtectedZones = true
+	mailboxDisabledTowns = "-1"
+
+
+	defaultPriority = "high"
+	niceLevel = 5
+	coresUsed = "-1"
+
+
+	optimizeDatabaseAtStartup = true
+	removePremiumOnInit = true
+	confirmOutdatedVersion = false
+
+
+	formulaLevel = 1.0
+	formulaMagic = 1.0
+	bufferMutedOnSpellFailure = false
+	spellNameInsteadOfWords = false
+	emoteSpells = true
+
+
+	allowChangeOutfit = true
+	allowChangeColors = true
+	allowChangeAddons = true
+	disableOutfitsForPrivilegedPlayers = false
+	addonsOnlyPremium = true
+
+
+	dataDirectory = "data/"
+	bankSystem = true
+	displaySkillLevelOnAdvance = false
+	promptExceptionTracerErrorBox = true
+	separateViplistPerCharacter = false
+	maximumDoorLevel = 500
+	maxMessageBuffer = 4
+
+
+	saveGlobalStorage = true
+	useHouseDataStorage = false
+	storePlayerDirection = false
+
+
+	checkCorpseOwner = true
+	monsterLootMessage = 3
+	monsterLootMessageType = 25
+
+
+	ghostModeInvisibleEffect = false
+	ghostModeSpellEffects = true
+
+
+	idleWarningTime = 14 * 60 * 1000
+	idleKickTime = 15 * 60 * 1000
+	expireReportsAfterReads = 1
+	playerQueryDeepness = 2
+	maxItemsPerPZTile = 0
+	maxItemsPerHouseTile = 0
+
+	
+	freePremium = true
+	premiumForPromotion = true
+
+
+	blessingOnlyPremium = true
+	blessingReductionBase = 30
+	blessingReductionDecreament = 5
+	eachBlessReduction = 8
+
+
+	experienceStages = false
+	rateExperience = 1000000.0
+	rateExperienceFromPlayers = 0
+	rateSkill = 7.0
+	rateMagic = 5.0
+	rateLoot = 3.0
+	rateSpawn = 1000
+
+
+	rateMonsterHealth = 10.0
+	rateMonsterMana = 4.0
+	rateMonsterAttack = 15.0
+	rateMonsterDefense = 6.0
+
+
+	minLevelThresholdForKilledPlayer = 0.9
+	maxLevelThresholdForKilledPlayer = 1.1
+
+
+	rateStaminaLoss = 1
+	rateStaminaGain = 3
+	rateStaminaThresholdGain = 12
+	staminaRatingLimitTop = 41 * 60
+	staminaRatingLimitBottom = 14 * 60
+	rateStaminaAboveNormal = 1.5
+	rateStaminaUnderNormal = 0.5
+	staminaThresholdOnlyPremium = true
+
+
+	experienceShareRadiusX = 30
+	experienceShareRadiusY = 30
+	experienceShareRadiusZ = 1
+	experienceShareLevelDifference = 2 / 3
+	extraPartyExperienceLimit = 20
+	extraPartyExperiencePercent = 5
+	experienceShareActivity = 2 * 60 * 1000
+
+
+	globalSaveEnabled = false
+	globalSaveHour = 8
+	shutdownAtGlobalSave = true
+	cleanMapAtGlobalSave = false
+
+
+	deSpawnRange = 2
+	deSpawnRadius = 50
+
+
+	maxPlayerSummons = 2
+	teleportAllSummons = false
+	teleportPlayerSummons = false
+
+
+	ownerName = "Admin"
+	ownerEmail = "Non Of Your Buisness"
+	url = "http://paradiseot.hopto.org/"
+	location = "Europe"
+	displayGamemastersWithOnlineCommand = true
+
+
+	adminLogsEnabled = false
+	displayPlayersLogging = true
+	prefixChannelLogs = ""
+	runFile = ""
+	outLogName = ""
+	errorLogName = ""
+	truncateLogsOnStartup = false
+dasnmsd,asdaksdahkjsadhkdsa
+asdujkashdsakjsadhkdjsa
+saddjsakhsdakjasdhjk
